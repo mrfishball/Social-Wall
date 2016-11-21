5133f0cf7741ce3c12d703de112a1234df099405
Steven Tuet Piu Kwok <mrfishball@gmail.com>
2016-11-21 05:42:25 -0500

    Added HTML and CSS mockup.

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,19 +1,137 @@
 import React from 'react';
-
+import ReactDOM from 'react-dom';
+// import spinner from './spin.svg';
 require('./App.css');
 
-export default () => <h1>Hello World</h1>;
+// Product list item image
+var ProductImage = React.createClass({
+  getDefaultProps: function() {
+  	var load = 'Loading...'
+    return {
+      loader: load,
+      showImage: false
+    };
+  },
 
-/*
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+  componentDidUpdate: function(prevProps) {
+    if (! this.props.showImages && prevProps.viewport) {
+      this.updatePosition();
+    }
+  },
 
-  render() {
+  updatePosition: function() {
+    var el = ReactDOM.findDOMNode(this);
+    this.props.updateImagePosition(el.offsetTop, el.offsetHeight);
+  },
+
+  render: function() {
+    var img = (this.props.showImage) ? this.props.src : this.props.loader;
     return (
-      <h1>Hello World</h1>
+      <img src={img} alt={this.props.alt} />
     );
   }
-}
-*/
+});
+
+// Product list item
+var Product = React.createClass({
+  getInitialState: function() {
+    return {
+      showImage: false
+    };
+  },
+
+  getDefaultProps: function() {
+    return {
+      showImage: false
+    };
+  },
+
+  componentWillMount: function() {
+    // allow image display override
+    if (this.props.showImage) {
+      this.setShowImage(true).bind(this);
+    }
+  },
+
+  updateImagePosition: function(top, height) {
+    // image is already displayed, no need to check anything
+    if (this.state.showImage) {
+      return;
+    }
+
+    // update showImage state if component element is in the viewport
+    var min = this.props.viewport.top;
+    var max = this.props.viewport.top + this.props.viewport.height;
+
+    if ((min <= (top + height) && top <= (max - 300))) {
+      this.setShowImage(true);
+    }
+  },
+
+  setShowImage: function(show) {
+    this.setState({
+      showImage: !!(show)
+    });
+  },
+
+  render: function() {
+    return (
+        <div className="block">
+          <ProductImage src={this.props.image} alt={this.props.title} viewport={this.props.viewport} showImage={this.state.showImage}
+            updateImagePosition={this.updateImagePosition} />
+          <div className="content">
+            <h2>{this.props.title}</h2>
+          </div>
+        </div>
+    );
+  }
+});
+
+// Product list
+var ProductList = React.createClass({
+  getInitialState: function() {
+    return {
+      viewport: {
+        top: 0,
+        height: 0
+      }
+    };
+  },
+
+  componentDidMount: function() {
+    window.addEventListener('scroll', this.updateViewport, false);
+    window.addEventListener('resize', this.updateViewport, false);
+    this.updateViewport();
+  },
+
+  componentWillUnmount: function() {
+    window.removeEventListener('scroll', this.updateViewport);
+    window.removeEventListener('resize', this.updateViewport);
+  },
+
+  updateViewport: function() {
+    // TODO: debounce this call
+    this.setState({
+      viewport: {
+        top: window.pageYOffset,
+        height: window.innerHeight
+      }
+    });
+  },
+
+  render: function() {
+    var self = this;
+
+    var itemViews = this.props.items.map(function(item) {
+      return <Product title={item.title} image={item.image} viewport={self.state.viewport} />
+    });
+
+    return (
+      <div className="wrapper">
+        {itemViews}
+      </div>
+    );
+  }
+});
+
+export default ProductList;
--- app/main.jsx
+++ app/main.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import ProductList from './components/App';
+require('./main.css');
+
+var items = [
+  { title: 'Kitten 1', image: 'http://placekitten.com/311/313' },
+  { title: 'Kitten 2', image: 'http://placekitten.com/302/302' },
+  { title: 'Kitten 3', image: 'http://placekitten.com/303/303' },
+  { title: 'Kitten 4', image: 'http://placekitten.com/304/304' },
+  { title: 'Kitten 5', image: 'http://placekitten.com/305/305' },
+  { title: 'Kitten 6', image: 'http://placekitten.com/306/306' },
+  { title: 'Kitten 7', image: 'http://placekitten.com/307/307' },
+  { title: 'Kitten 8', image: 'http://placekitten.com/308/308' },
+  { title: 'Kitten 9', image: 'http://placekitten.com/310/310' },
+  { title: 'Kitten 10', image: 'http://placekitten.com/311/311' }
+];
 
 ReactDOM.render(
-  <App />,
-  document.body.appendChild(document.createElement('div'))
+  <ProductList items={items}/>,
+  document.getElementById('app')
 );
